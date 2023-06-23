946f79ae2ebd690a659054ccefbf84be686ed95c
Xwcccc <96057280+Xwcccc@users.noreply.github.com>
2023-06-23 22:22:54 +0800

    Add files via upload

---
+++ js/locate.js
@@ -0,0 +1,7 @@
+navigator.geolocation.getCurrentPosition(function showPosition(position){
+    const lat = position.coords.latitude; //纬度
+    const lot = position.coords.longitude; //经度
+});
+
+export {
+}
